b46531f37796df74c222148d50e459fbf3a321d3
Devesh Avutu <devesh14may@gmail.com>
2020-04-12 13:34:45 +0530

    initialising server side render react app with boiler plate code

---
+++ server/src/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const React = require("react");
+const renderToString = require("react-dom/server").renderToString;
+const Home = require("./client/components/Home").default;
+const app = express();
+
+app.get("/", (req, res) => {});
+app.listen(3000, () => {
+	console.log("listening to port 3000");
+});
