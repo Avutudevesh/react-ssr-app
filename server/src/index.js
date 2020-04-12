597d71353bdf0cc5f6474ecb1cf2fb97cde0c986
Devesh Avutu <devesh14may@gmail.com>
2020-04-12 23:31:56 +0530

    setting up react router

--- server/src/index.js
+++ server/src/index.js
@@ -2,8 +2,8 @@ import express from "express";
 import rendered from "./helpers/renderer";
 const app = express();
 app.use(express.static("public"));
-app.get("/", (req, res) => {
-	res.send(rendered());
+app.get("*", (req, res) => {
+	res.send(rendered(req));
 });
 app.listen(3000, () => {
 	console.log("listening to port 3000");
