1d1183c4c540b784c04cbf7a2301833a965afb62
Devesh Avutu <devesh14may@gmail.com>
2020-04-12 21:08:13 +0530

    setting up server and client scripts

---
+++ server/src/client/client.js
@@ -0,0 +1,6 @@
+//startup point for client application
+import React from "react";
+import ReactDom from "react-dom";
+import Home from "./components/Home";
+
+ReactDom.hydrate(<Home />, document.querySelector("#root"));
--- server/src/client/components/Home.js
+++ server/src/client/components/Home.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Home = () => {
-	return <div>I am home component</div>;
+	return (
+		<div>
+			<div>I am best home component</div>
+			<button onClick={() => console.log("I am clicked")}>Press me</button>
+		</div>
+	);
 };
 
 export default Home;
