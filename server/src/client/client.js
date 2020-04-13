b760c07da64495782ea96d932116eb780a7769ea
Devesh Avutu <devesh14may@gmail.com>
2020-04-13 14:18:50 +0530

    setting up reducers and action creaters

--- server/src/client/client.js
+++ server/src/client/client.js
@@ -1,12 +1,19 @@
 //startup point for client application
 import React from "react";
 import ReactDom from "react-dom";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
 import { BrowserRouter } from "react-router-dom";
 import Routes from "./Routes";
+import reducers from "./reducers";
 
+const store = createStore(reducers, {}, applyMiddleware(thunk));
 ReactDom.hydrate(
-	<BrowserRouter>
-		<Routes />
-	</BrowserRouter>,
+	<Provider store={store}>
+		<BrowserRouter>
+			<Routes />
+		</BrowserRouter>
+	</Provider>,
 	document.querySelector("#root")
 );
--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,11 @@
 import express from "express";
 import rendered from "./helpers/renderer";
+import createStore from "./helpers/createStore";
 const app = express();
 app.use(express.static("public"));
 app.get("*", (req, res) => {
-	res.send(rendered(req));
+	const store = createStore();
+	res.send(rendered(req, store));
 });
 app.listen(3000, () => {
 	console.log("listening to port 3000");
