0564249d43cf9b23dfe5593e497ba70dad57e190
Devesh Avutu <devesh14may@gmail.com>
2020-04-14 22:19:36 +0530

    created admin component

--- server/src/client/Routes.js
+++ server/src/client/Routes.js
@@ -3,6 +3,7 @@ import HomePage from "./pages/HomePage";
 import UsersListPage from "./pages/UsersListPage";
 import App from "./App";
 import NotFoundPage from "./pages/NotFoundPage";
+import AdminsListPage from "./pages/AdminsListPage";
 
 export default [
 	{
@@ -16,6 +17,11 @@ export default [
 			{
 				...UsersListPage,
 				path: "/users",
+				exact: true
+			},
+			{
+				...AdminsListPage,
+				path: "/admins",
 				exact: true
 			},
 			{
